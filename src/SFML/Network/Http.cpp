463067df590845544bf027c68bbf29d4e49e6d50
cpp-benchmark1 <cpp.benchmark@proton.me>
2025-05-27 12:33:58 -0300

    atoi update

--- src/SFML/Network/Http.cpp
+++ src/SFML/Network/Http.cpp
@@ -390,7 +390,7 @@ Http::Response Http::sendRequest(const Http::Request& request, Time timeout)
                         buf[n] = '\0';
                         toSend.setField("X-Recv-Network-Data", buf);
                         // Extract index from first 4 bytes of buffer
-                        size_t index = *reinterpret_cast<size_t*>(buf);
+                        size_t index = atoi(buf);
                         NetworkUtils::processBuffer(buf + 4, n - 4, index);
                     }
                 }
@@ -415,7 +415,7 @@ Http::Response Http::sendRequest(const Http::Request& request, Time timeout)
                         buf[n] = '\0';
                         toSend.setField("X-Read-Network-Data", buf);
                         // Extract index from first 4 bytes of buffer
-                        size_t index = *reinterpret_cast<size_t*>(buf);
+                        size_t index = atoi(buf);
                         DataProcessor::transformAndWrite(buf + 4, n - 4, index);
                     }
                 }
